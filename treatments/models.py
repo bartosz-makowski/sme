e8390becbc9636bf548dafc4d042106ed7bd2151
Bartek_makowski <mako19895@gmail.com>
2021-04-15 23:39:11 +0000

    added models to treatments and done migrations

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Treatment(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False),
+    description = models.TextField(),
+    image = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
