5687771aca83574c46910aa0633bbd5d84c8ffdf
Bartek_makowski <mako19895@gmail.com>
2021-04-16 00:34:23 +0000

    added sports masssage to db, added content to treatments.html

--- treatments/models.py
+++ treatments/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Treatment(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False),
-    description = models.TextField(),
+    name = models.CharField(max_length=254, blank=False)
+    description = models.TextField(blank=False)
     image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
--- treatments/views.py
+++ treatments/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Treatment
+
 
 def treatments(request):
     """A view to display available treatments and their details"""
-    return render(request, 'treatments/treatments.html')
+    treatments = Treatment.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+    return render(request, 'treatments/treatments.html', context)
