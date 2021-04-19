87b2bc3a056846d8d62192e8bcf1fa9ff36b211c
Bartek_makowski <mako19895@gmail.com>
2021-04-19 23:26:42 +0000

    added boolean to treatments models, added 3 treatments via admin panel

--- treatments/models.py
+++ treatments/models.py
@@ -5,6 +5,7 @@ class Treatment(models.Model):
     name = models.CharField(max_length=254, blank=False)
     description = models.TextField(blank=False)
     image = models.ImageField(null=False, blank=False)
+    featured = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
