83b5df87ffc87875de097f8e9ecea415811b63fb
Bartek_makowski <mako19895@gmail.com>
2021-04-20 01:17:13 +0000

    start and setup of reviews app

---
+++ reviews/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=50, null=False,
+                             blank=False, default='Title')
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=20, null=False, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ reviews/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# from .models import Review
+
+def reviews(request):
+    """A view to display a reviews page with all reviews from the DB """
+    # reviews = Review.object.all()
+
+    #context = {
+    #    'reviews': reviews,
+    #}
+    return render(request, 'reviews/reviews.html',)
