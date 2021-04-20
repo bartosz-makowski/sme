cec1d860bcd148e346d9b800ef660eb0153016b4
Bartek_makowski <mako19895@gmail.com>
2021-04-20 00:10:37 +0000

    added logic to display a featured trreatment on index.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
+from treatments.models import Treatment
+
 
 def index(request):
     """A view to display index page"""
-    
-    return render(request, 'home/index.html')
+    treatments = Treatment.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+    return render(request, 'home/index.html', context)
