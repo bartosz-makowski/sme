d3378d5a32c187ed9aa6b8f4644d261a2768a82c
Bartek_makowski <mako19895@gmail.com>
2021-04-22 21:25:03 +0000

    added logic to display reviews at index.html

--- home/views.py
+++ home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
 from treatments.models import Treatment
+from reviews.models import Review
 
 
 def index(request):
     """A view to display index page"""
     treatments = Treatment.objects.all()
+    reviews = Review.objects.all()
 
     context = {
         'treatments': treatments,
+        'reviews': reviews,
     }
     return render(request, 'home/index.html', context)
