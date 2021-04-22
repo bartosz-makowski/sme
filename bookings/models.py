61db680686c693f09d9cf75fd01a6bf14bddb673
Bartek_makowski <mako19895@gmail.com>
2021-04-22 23:04:05 +0000

    added Time model to bookings

--- bookings/models.py
+++ bookings/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Day(models.Model):
     date = models.DateField(auto_now=False,
                             auto_now_add=False, primary_key=True)
+
+
+class Time(models.Model):
+    date = models.ForeignKey(Day, on_delete=models.CASCADE)
+    time = models.CharField(max_length=12)
