de4ce64c7406fd8bfeb4253819d2bff1af7b39a6
Bartek_makowski <mako19895@gmail.com>
2021-04-22 21:27:22 +0000

    added boolean feautered to reviews models and made migrations

--- reviews/models.py
+++ reviews/models.py
@@ -7,6 +7,7 @@ class Review(models.Model):
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=20, null=False, blank=True)
-
+    featured = models.BooleanField(default=False, null=True, blank=True)
+    
     def __str__(self):
         return self.title
