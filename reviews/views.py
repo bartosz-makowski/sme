5de3811924e6e898fbbd0d96f81fdbcac337d937
Bartek_makowski <mako19895@gmail.com>
2021-04-22 15:25:27 +0000

    added logic to display reviews @reviews.html, added reviews.css

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
-# from .models import Review
+from .models import Review
+
 
 def reviews(request):
     """A view to display a reviews page with all reviews from the DB """
-    # reviews = Review.object.all()
+    reviews = Review.objects.all()
 
-    #context = {
-    #    'reviews': reviews,
-    #}
-    return render(request, 'reviews/reviews.html',)
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'reviews/reviews.html', context)
