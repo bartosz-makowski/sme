a18528e3faf8ca826cfa6b72357e971036e3d2a2
Bartek_makowski <mako19895@gmail.com>
2021-04-23 22:45:12 +0000

    finished setting up signals for orderlineitem

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,7 +3,16 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+@receiver(post_save, sender=OrderLineItem)
 def updateOnSave(sender, instance, created, **kwargs):
     """
-    update order total
+    update order total on lineitem update/create
     """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def updateOnSave(sender, instance, created, **kwargs):
+    """
+    update order total on lineitem delete
+    """
+    instance.order.update_total()
