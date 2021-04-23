c50e41e370e837824ca8d05185fdf8d66c07c695
Bartek_makowski <mako19895@gmail.com>
2021-04-23 21:24:17 +0000

    add location model to home app, styled location section at index.html

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    description = models.TextField()
+    image = image = models.ImageField(null=False, blank=False)
+
--- home/views.py
+++ home/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 from deals.models import Deal
 from reviews.models import Review
+from .models import Location
 
 
 def index(request):
     """A view to display index page"""
     deals = Deal.objects.all()
     reviews = Review.objects.all()
+    location = Location.objects.get()
 
     context = {
         'deals': deals,
         'reviews': reviews,
+        'location': location
     }
     return render(request, 'home/index.html', context)
