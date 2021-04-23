e29f1d1bedb57ce38d707068eb604ed15d331a58
Bartek_makowski <mako19895@gmail.com>
2021-04-23 23:04:57 +0000

    added forms for checkout, added basic content to checkout.html

---
+++ checkout/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number'
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
