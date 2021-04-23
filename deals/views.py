eaf330764ea3dad32909f229513c4f43237d2bcc
Bartek_makowski <mako19895@gmail.com>
2021-04-23 13:54:15 +0000

    added content and styling of deals.html

--- deals/views.py
+++ deals/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Deal
+
 
 def deals(request):
     """A view to display deals page """
-    return render(request, 'deals/deals.html')
+
+    deals = Deal.objects.all()
+
+    context = {
+        'deals': deals,
+    }
+    return render(request, 'deals/deals.html', context)
