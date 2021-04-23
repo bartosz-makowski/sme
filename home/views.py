5e85719395d1cec92e1cedaf04582ce03140021e
Bartek_makowski <mako19895@gmail.com>
2021-04-23 20:49:47 +0000

    changed deal to be featured on homepage

--- home/views.py
+++ home/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
-from treatments.models import Treatment
+from deals.models import Deal
 from reviews.models import Review
 
 
 def index(request):
     """A view to display index page"""
-    treatments = Treatment.objects.all()
+    deals = Deal.objects.all()
     reviews = Review.objects.all()
 
     context = {
-        'treatments': treatments,
+        'deals': deals,
         'reviews': reviews,
     }
     return render(request, 'home/index.html', context)
