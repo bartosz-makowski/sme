a8241eebfaad8f8fc3608da5902eea465c16bc6c
Bartek_makowski <mako19895@gmail.com>
2021-04-23 11:30:37 +0000

    deals model created and migration finished

--- deals/models.py
+++ deals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Deal(models.Model):
+    name = models.CharField(max_length=65)
+    price = models.IntegerField(max_length=3)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    featured = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
