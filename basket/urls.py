e8601d6ff1d4eb282bf0b22b44d107bbb1c6649d
Bartek_makowski <mako19895@gmail.com>
2021-04-24 10:31:24 +0000

    when button for a deal is pressed item is pushed to session

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('basket', views.basket, name='basket')
+    path('basket', views.basket, name='basket'),
+    path('add/<deal_id>/', views.add_to_basket, name='add_to_basket')
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def basket(request):
     """A view to display basket page """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, deal_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if deal_id in list(basket.keys()):
+        basket[deal_id] += quantity
+    else:
+        basket[deal_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
