d2577967d710d23c6c8ed997a30c8a9feac1bbaf
Bartek_makowski <mako19895@gmail.com>
2021-04-24 11:10:44 +0000

    basket content from session displaying on basket.html

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,8 +1,22 @@
+from django.shortcuts import get_object_or_404
+from deals.models import Deal
+
+
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Deal, pk=item_id)
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product
+        })
 
     context = {
         'basket_items': basket_items,
