b0c4371bd2a65348cd88f7c36af3ee4229dba351
Bartek_makowski <mako19895@gmail.com>
2021-04-25 09:45:30 +0000

    changed STATICFILES_DIRS to _ROOT to try and push to heroku

--- sme/settings.py
+++ sme/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&uqj+laxm+odbnl-g@sorr&h)d8y*!hi4x3m$eygc42_3_6%=7'
+os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -159,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_ROOT = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
