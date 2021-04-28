dae2e3f24a9b1d11d6c539d17e2830ed8b27690b
Bartek_makowski <mako19895@gmail.com>
2021-04-28 11:25:17 +0000

    added webhooks handling to checkout app

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,9 +5,21 @@ class StripeWH_Handler:
     """Handle Stripe webhooks """
     def __init__(self, request):
         self.request = request
-    
+
     def handle_event(self, event):
         """Handle a generic/unknown/unexpected webhook event """
         return HttpResponse(
-            content=f'Webhook receive: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle a  webhook event payment_intent.succeeded"""
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_failed(self, event):
+        """Handle a  webhook event payment_intent.failed"""
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
             status=200)
